12cfa7456442e998e7935f6c15e8b8431931b609
Jazzmyne Roberson <jroberson0927@gmail.com>
2024-09-14 14:40:41 -0400

    Create updateStock function

--- inventory_management.js
+++ inventory_management.js
@@ -19,5 +19,20 @@ function displayProductDetails(inventory) {
             Stock Status: ${stockStatus}`
 } // Function to determine whether a product is in stock, and displays it in the console.console.log(displayProductDetails(inventory.food);
 
-
 // console.log(displayProductDetails[0]); // Output should display details for "food".
+
+
+// TASK 3: Create a Function to Update Product Stock After Sales
+
+function updateStock(inventory, unitsSold) {
+    inventory.quantity -= unitsSold;
+    if (inventory.quantity <=0) {
+        return "Out of Stock."
+    } else if (inventory.quantity <= inventory.lowStockLevel) {
+        return `Low Stock. New quantity: ${inventory.quantity}.`
+    } else {
+        return `In Stock. New quantity: ${inventory.quantity}.`
+    }
+} // Updates the quantity in stock and returning the new quantity.
+
+// console.log(updateStock(inventory[0], 15)); // Output should display the new quantity and stock status.
